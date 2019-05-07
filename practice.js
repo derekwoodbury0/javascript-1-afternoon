3421f8400463536d3b287979f8b5c8cf629733f8
Derek <Derek@Dereks-MacBook-Pro.local>
2019-05-07 14:37:28 -0600

    completed through problem 12

--- practice.js
+++ practice.js
@@ -83,7 +83,13 @@ var devMountainClassPet = dog.name
 // If the name parameter is anything else, return 'Cool name, NAMEPARAM'
 // with NAMEPARAM being the name parameter being passed in
 
-// Code here
+function nameCheck(name) {
+  if (name === 'Steven') {
+    return 'What is up Steven?'
+  } else if (name === 'Bryan') {
+    return 'Hey Bryan!'
+  } else return 'Cool name, ' + name
+}
 
 //////////////////PROBLEM 11////////////////////
 
@@ -91,12 +97,14 @@ var devMountainClassPet = dog.name
 // that will be numbers.
 // The add function should return the two parameters added together
 
-//Code Here
+function add(num1, num2) {
+  return num1 + num2
+}
 
 //Now invoke add, passing in the numbers 3 and 4
 //storing the result in the variable mathSum.
 
-//Code Here
+var mathSum = add(3,4)
 
 //////////////////PROBLEM 12////////////////////
 
@@ -107,7 +115,15 @@ var devMountainClassPet = dog.name
 // If the passed in color equals 'black', return 'so trendy'
 // Otherwise, you should return the string 'you need to evaluate your favorite color choice'
 
-// Code here
+function faveColorFinder(color) {
+  if (color === 'red') {
+    return 'red is a great color'
+  } else if (color === 'green') {
+    return 'green is a solid favorite color'
+  } else if (color === 'black') {
+    return 'so trendy'
+  } else return 'you need to evaluate your favorite color choice'
+}
 
 //////////////////PROBLEM 13////////////////////
 
